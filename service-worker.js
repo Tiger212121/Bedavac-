b0d7a9dbc228f681e4b0643f34460571a2dabdca
Tiger212121 <j94013201@gmail.com>
2025-09-04 16:59:28 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,7 @@
+// Bu boş service worker dosyası, tarayıcının uygulamayı
+// kurulabilir (installable) olarak algılaması için yeterlidir.
+// Şimdilik çevrimdışı özellikler eklemeyeceğiz.
+
+self.addEventListener('fetch', (event) => {
+  // Bu olay dinleyici, PWA'nın çalışması için gereklidir.
+});
